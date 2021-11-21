34ce238ff15f72565b2296d5b4d08801d4ec0c2e
djouamaah <djouamaah@gmail.com>
2021-11-21 02:14:14 +0100

    routes in app/Admin/routes.php; add admin configuration in filesystems.php

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -12,5 +12,12 @@ Route::group([
 ], function (Router $router) {
 
     $router->get('/', 'HomeController@index')->name('home');
+    $router->get('/users', 'UserController@grid')->name('users');
+    $router->get('/accounting', 'AccountingController@grid')->name('accounting');
+    $router->get('/items', 'ItemsController@grid')->name('items');
+    $router->get('/archive', 'ArchiveController@grid')->name('archive');
+    $router->get('/messages', 'MessagesController@grid')->name('messages');
+    $router->get('/settings', 'SettingsController@grid')->name('settings');
+
 
 });
